c72e327b51fe6dc3a1ee98f4381d74239ff768f1
Mithun <Mithun@Mithun-PC>
2021-12-10 10:38:00 +0530

    commit num 1

---
+++ src/test/java/com/practice/Tester.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class Tester {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Demo 1 Started");
+		System.out.println("Demo 2 Started");
+		
+	}
+
+
+}
